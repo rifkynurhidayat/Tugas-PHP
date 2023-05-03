29491c151ff0c256573cfa2f732920ffa4d74079
Kuninganunited20 <rifkynurhidayat623@gmail.com>
2023-05-03 20:49:32 +0700

    praktek 3

---
+++ praktek3.php
@@ -0,0 +1,6 @@
+<?php
+    $mahasiswa = ['Nama : Ronaldo','Nama : Messi', 'Nama : Rashford','Nama : Luke shaw','Nama : Shanco'];
+    for ($i=0; $i<count($mahasiswa); $i++) { 
+        echo $mahasiswa[$i]."<br>";
+    }
+?>
