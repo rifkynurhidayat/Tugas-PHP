8aa41a56a0bf3467451fb47ac2c0525dc3c0d357
Kuninganunited20 <rifkynurhidayat623@gmail.com>
2023-05-05 22:57:26 +0700

    revisi dikit

--- praktek4.php
+++ praktek4.php
@@ -1,7 +1,3 @@
-<?php
-        $json_string = file_get_contents('data.json');
-        $json = json_decode($json_string,true);
-?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -24,7 +20,6 @@
     <h2>Daftar Nilai</h2>
 <table border="1" cellpadding="18" cellspacing="0" style="text-align: center;">
         <tr>
-            <td style=" font-weight: bold;">No.</td>
             <td style=" font-weight: bold;">Nama</td>
             <td style=" font-weight: bold;">Tanggal Lahir</td>
             <td style=" font-weight: bold;">Umur</td>
@@ -33,24 +28,40 @@
             <td style=" font-weight: bold;">Nilai</td>
             <td style=" font-weight: bold;">Hasil</td>
         </tr>
+        <?php
+        $json_string = file_get_contents('data.json');
+        $json = json_decode($json_string,true);
+        foreach ($json as $data){
+            $tanggal_lahir = $data['tanggal_lahir'];
+            $umur = new DateTime($tanggal_lahir);
+            $sekarang = new DateTime();
+            $usia = $sekarang->diff($umur);
+            $nilai = $data["nilai"];
+            if ($nilai >=90 ){
+                $hasil = "A";
+            }else if($nilai >=80){
+                $hasil= "B";
+            }else if($nilai >=70){
+                $hasil= "C";
+            }else if ($nilai<=65){
+               $hasil= "D";
+            }else{
+                $hasil="E";
+            }
+          
 
-        <tr style='background-color:aquamarine'>
-        <?php $i=1; ?>
-        <?php foreach ($json as $data) : ?>
-            <td><?= $i; ?></td>
+        ?>
+        <tr>
             <td><?= $data["nama"]; ?></td>
             <td><?= $data["tanggal_lahir"]; ?></td>
-            <td><?= $data["umur"]; ?></td>
+            <td><?= $usia->y."&nbsp"."Tahun" ?></td>
             <td><?= $data["alamat"]; ?></td>
             <td><?= $data["kelas"]; ?></td>
-            <td><?= $data["nilai"]; ?></td>
-            <td><?= $data["hasil"]; ?></td>
+            <td><?= $nilai; ?></td>
+            <td><?= $hasil; ?></td>
         </tr>
-        <?php $i++; ?>
-        <?php endforeach; ?>
-
+     <?php } ?>
     </table>
-    
 </body>
 </html>
   
